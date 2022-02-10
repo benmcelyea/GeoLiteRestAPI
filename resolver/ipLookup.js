7cb64c25779cffe7902495e571063dc1e7ec57c1
Ben McElyea <bmcelyea@gmail.com>
2022-02-09 23:35:57 -0500

    Tweaked response messages

--- resolver/ipLookup.js
+++ resolver/ipLookup.js
@@ -9,27 +9,23 @@ const cityReader = Reader.openBuffer(cityDBbuffer);
 //Private IP address space regex to filter out private address ranges - Maxmind does not resolve private Ip address spaces 
 const privateIPregex = new RegExp('(^127\\.)|(^10\\.)|(^172\\.1[6-9]\\.)|(^172\\.2[0-9]\\.)|(^172\\.3[0-1]\\.)|(^192\\.168\\.)')
 
-
 function lookupIp(ip) {
+
   let city = {}
-  let isp = {}
   let status = {
-
-    "cityStatus": "OK",
-
+    responseCode: 200,
+    message: "Ok"
   }
 
   if (!net.isIP(ip)) {
 
-    status.result = "BADREQUEST"
-    status.code = "400"
+    status.responseCode = 400
     status.message = "Invalid IP detected"
-    status.cityStatus = "BADREQUEST"
 
 
     return {
       status,
-      city,
+      results,
 
     }
   }
@@ -37,15 +33,14 @@ function lookupIp(ip) {
 
   if (privateIPregex.test(ip)) {
 
-    status.result = "LOCALIP"
-    status.code = "200"
+    status.responseCode = 400
     status.message = "Local IP range deteced. Maxmind does not resolve local ip address ranges "
-    status.cityStatus = "LOCALIP"
+
 
 
     return {
       status,
-      city,
+      results,
 
     }
   }
@@ -54,24 +49,22 @@ function lookupIp(ip) {
   // Lookup ip in the city reader database
   try {
 
-    city = cityReader.city(ip)
+    results = cityReader.city(ip)
+    status.responseCode = 200
+
 
   } catch (AddressNotFoundError) {
 
+    status.responseCode = 400
     status.message = "IP not found in the GeoLite2 City database"
-    status.cityStatus = "NOTFOUND"
-
   }
 
-
-  let results = {
+  let lookupResults = {
     status,
-    city,
-
+    results,
   }
 
-  return (results)
+  return (lookupResults)
 }
-
 
 module.exports = { lookupIp }
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,16 +4,28 @@ var router = express.Router();
 const net = require('net');
 
 
-/* GET home page. */
+//GET Request to lookup an IP address
 router.get("/v1/ip/:ip", function (req, res, next) {
   const ip = req.params.ip;
 
   if (net.isIP(ip)) {
+    results = lookupIp.lookupIp(ip)
 
-    res.send(lookupIp.lookupIp(ip));
+    if (results.status.responseCode === 200) {
+      res.send(lookupIp.lookupIp(ip));
+    } else if (results.status.responseCode === 400) {
+      res.status(400).send(results)
+    }
+
+
   } else {
 
-    res.status(400).json({ error: 'Invalid IP address' });
+    res.status(400).json({
+      status: {
+        "responseCode": "400",
+        "message": "Invalid IP address detected"
+      },
+    });
 
   }
 
